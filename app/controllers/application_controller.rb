d494182a2423c3b73c168dbadf7d8700ad58d1d2
sarah schaab <sarahschaab@me.com>
2017-09-29 20:48:43 -0400

    tweets_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::API
+  protect_from_forgery
+  before_action :load_tweets
+  before_action :search_tweets
+
+
+  def load_tweets
+    @tweets = Rails.application.config.assets.twitter_client.user_timeline('BTCTN', count: 20)
+  end
+
+  def search_tweets
+    @search = Rails.application.config.assets.twitter_client.search('#cryptocurrency -rt', lang: "en", count: 20 )
+  end
 end

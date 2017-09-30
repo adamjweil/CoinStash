1ea5db645e99d3dfd021e1a1d9e9c85636f98eee
Adam Weil <adamjweil@gmail.com>
2017-09-30 19:19:50 -0400

    yday prices update

---
+++ CoinStashReact/NavComponent/TweetsComponent.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+export default class TweetsComponent extends Component {
+  constructor() {
+    super();
+    this.state = {
+      tweet0: [],
+      tweet1: [],
+      tweet2: [],
+      tweet3: [],
+    }
+  }
+  componentDidMount() {
+    this.getCurrentTweets();
+    setInterval(this.getCurrentTweets, 100000);
+  }
+
+  getCurrentTweets = () => {
+    fetch('http://localhost:3000/tweets')
+    .then(function(response) {
+      return response.json();
+    })
+    .then((obj) => {
+      console.log(obj[0]);
+      console.log(obj[0].user.name);
+      this.setState({tweet0: obj[0]})
+      this.setState({tweet1: obj[1]})
+      this.setState({tweet2: obj[2]})
+      this.setState({tweet3: obj[3]})
+    })
+  }
+  render() {
+    const { title, description, url, urlToImage, author } = this.props
+    return (
+      <View>
+        <Text style={styles.tweetContainer}>
+          {this.state.tweet0.text}
+        </Text>
+
+        <Text style={styles.tweetContainer}>
+          {this.state.tweet1.text}
+        </Text>
+
+        <Text style={styles.tweetContainer}>
+          {this.state.tweet2.text}
+        </Text>
+
+        <Text style={styles.tweetContainer}>
+          {this.state.tweet3.text}
+        </Text>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+    width: 300,
+    height: 300,
+  },
+  tweetContainer: {
+    fontSize: 20,
+    textAlign: 'left',
+    margin: 10,
+    width: 300,
+    height: 100,
+  },
+});
--- CoinStashReact/pages/ltc.js
+++ CoinStashReact/pages/ltc.js
@@ -16,54 +16,46 @@ import {
   Text,
   View
 } from 'react-native';
-// var MarqueeLabel = require('@remobile/react-native-marquee-label');
 
 export default class ltc extends Component {
   constructor() {
     super();
-    console.log("hi")
     this.state = {
-      bitcoinPrice: "",
-      bitcoinYdayPrice: "",
-      ethereumPrice: "",
-      ethereumYdayPrice: "",
       liteCoinPrice: "",
       liteCoinYdayPrice: ""
     };
   }
 
   getCurrentPrice = () => {
-    // console.log("hi")
     fetch('https://api.lionshare.capital/api/prices')
     .then(function(response) {
-      // debugger
       return response.json()
     }).then((obj) => {
-      console.log(this)
-      console.log(obj)
-      console.log(obj.data.BTC.length)
-      console.log(obj.data.ETH.length)
-      console.log(obj.data.LTC.length)
-      this.setState({bitcoinPrice: obj.data.BTC[obj.data.BTC.length - 1],
-                    ethereumPrice: obj.data.ETH[obj.data.ETH.length - 1],
-                    liteCoinPrice: obj.data.LTC[obj.data.LTC.length - 1]})
-
-                  })
+      this.setState({liteCoinPrice: obj.data.LTC[obj.data.LTC.length - 1]})
+    })
   }
+
+  getYesterdayPrice = () => {
+    fetch('https://www.bitstamp.net/api/v2/ticker/ltcusd')
+    .then(function(response) {
+      return response.json();
+    })
+    .then((obj => {
+      this.setState({liteCoinYdayPrice: obj.open})
+    }))
+  }
+
   componentDidMount() {
     this.getCurrentPrice()
     setInterval(this.getCurrentPrice, 100000);
+    this.getYesterdayPrice()
+    setInterval(this.getYesterdayPrice, 100000);
 
-    // Yesterday's Bitcoin Price
-    // fetch('https://api.coindesk.com/v1/bpi/currentprice.json')
-    // .then(function(response) {
-    //   return response.json()
-    // }).then((obj) => {
-    //   console.log(JSON.parse(obj))
-    //   this.setState({bitcoinYdayPrice: obj.data})
-    // });
-
-
+    let today = this.state.liteCoinPrice
+    let yday = Math.round(this.state.liteCoinYdayPrice)
+    let diff = this.state.liteCoinPrice - this.state.liteCoinYdayPrice
+    let change = diff.toFixed(2);
+    let colorBool = (change >= 0) ? "green" : "red";
   }
 
   // componentWillUnmount() {
@@ -75,9 +67,15 @@ export default class ltc extends Component {
   };
 
   render() {
-    // debugger
+    let today = this.state.liteCoinPrice
+    let yday = Math.round(this.state.liteCoinYdayPrice)
+    let diff = this.state.liteCoinPrice - this.state.liteCoinYdayPrice
+    let change = diff.toFixed(2);
+    let colorBool = (change >= 0) ? "green" : "red";
+
     const { bitcoinPrice } = this.state
     return (
+
       <View style={styles.container}>
         <View style={{
           marginTop: 10,
@@ -90,9 +88,17 @@ export default class ltc extends Component {
               justifyContent: 'center'
             }}>LTC</Text>
           </View>
-          <Text style={{
-              fontSize: 30
-            }}>{`$${this.state.liteCoinPrice}`}</Text>
+
+          <Text style={{fontSize: 30}}>{`$${this.state.liteCoinPrice}`}{'\n'}<Text
+            style={styles.yDay}>Yesterday EOD: ${this.state.liteCoinYdayPrice}</Text>
+          </Text>
+
+            <Text style={styles.yDayPrice}>
+              <Text style={{color: colorBool}}>
+                  Daily Change: ${change}
+              </Text>
+            </Text>
+
       </View>
     );
   }
@@ -125,12 +131,15 @@ const styles = StyleSheet.create({
     backgroundColor: 'blue',
     width:400,
     height:50,
-    // fontSize:12,
-    // fontWeight:'800',
-    // color:'white',
   },
   header: {
     backgroundColor: 'blue'
+  },
+  yDay: {
+    fontSize: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    textAlign: 'center',
   }
 });
 

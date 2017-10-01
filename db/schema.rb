64fc192840a2fc279cb0b735602c2dea3307208d
sarah schaab <sarahschaab@me.com>
2017-10-01 16:07:31 -0400

    change seeds to match DB

--- db/schema.rb
+++ db/schema.rb
@@ -16,8 +16,8 @@ ActiveRecord::Schema.define(version: 20170928205122) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "first_name", null: false
-    t.string "last_name", null: false
+    t.string "firstName", null: false
+    t.string "lastName", null: false
     t.string "email", null: false
     t.string "password_digest", null: false
     t.datetime "created_at", null: false

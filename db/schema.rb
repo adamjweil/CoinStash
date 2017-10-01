5aca238174755fcdd49432ba56352bd3372d5b7b
sarah schaab <sarahschaab@me.com>
2017-10-01 16:06:56 -0400

    change seeds to match DB

--- db/schema.rb
+++ db/schema.rb
@@ -20,7 +20,6 @@ ActiveRecord::Schema.define(version: 20170928205122) do
     t.string "last_name", null: false
     t.string "email", null: false
     t.string "password_digest", null: false
-    t.string "state", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
